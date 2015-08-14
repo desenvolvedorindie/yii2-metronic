314ec404ef6b639e96f1d63e50cadd5922c5ba6c
= <wfcreations@gmail.com>
2015-08-14 06:47:08 -0300

    fix ignore

---
+++ assets/dist/js/init.js
@@ -0,0 +1,7 @@
+(function () {
+    jQuery(document).ready(function () {
+        Metronic.init(); // init metronic core componets
+        Layout.init(); // init layout
+        QuickSidebar.init(); // init quick sidebar
+    });
+})();
