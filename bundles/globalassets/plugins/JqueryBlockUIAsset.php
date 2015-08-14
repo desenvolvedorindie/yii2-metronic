e386de57ac88c120275b34b3c73dd9d61e85ac40
= <wfcreations@gmail.com>
2015-08-14 07:33:41 -0300

    change blockui asset class, add slimscroll asset

---
+++ bundles/globalassets/plugins/JqueryBlockUIAsset.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace wfcreations\metronic\bundles\globalassets\plugins;
+
+use yii\web\AssetBundle;
+
+class JqueryBlockUIAsset extends AssetBundle {
+
+    public $sourcePath = '@bower/blockui';
+    public $js = [
+        'jquery.blockUI.js'
+    ];
+
+}
--- bundles/globalassets/plugins/JqueySlimScrool.php
+++ bundles/globalassets/plugins/JqueySlimScrool.php
@@ -4,11 +4,11 @@ namespace wfcreations\metronic\bundles\globalassets\plugins;
 
 use yii\web\AssetBundle;
 
-class BlockUIAsset extends AssetBundle {
+class JqueySlimScrool extends AssetBundle {
 
-    public $sourcePath = '@bower/blockui';
+    public $sourcePath = '@bower/slimscroll';
     public $js = [
-        'jquery.blockUI.js'
+        'jquery.slimscroll.min.js'
     ];
 
 }
