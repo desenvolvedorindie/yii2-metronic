d9066557e77fc60b71d280201f47da3112ab0dca
= <wfcreations@gmail.com>
2015-08-24 17:59:31 -0300

    fix core dependency

--- bundles/CoreAsset.php
+++ bundles/CoreAsset.php
@@ -15,7 +15,7 @@ class CoreAsset extends AssetBundle {
         'wfcreations\metronic\bundles\FontAsset',
         'yii\web\JqueryAsset',
         'wfcreations\metronic\bundles\globalassets\plugins\JqueryBlockUIAsset',
-        'wfcreations\metronic\bundles\globalassets\plugins\JqueySlimScrool',
+        'wfcreations\metronic\bundles\globalassets\plugins\JqueySlimScroolAsset',
         //'wfcreations\metronic\bundles\globalassets\JqueryMigrateAsset',
         'yii\jui\JuiAsset',
         'rmrevin\yii\fontawesome\AssetBundle',
