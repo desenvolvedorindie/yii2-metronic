6435f72cd21ca997d8d9a2aea5dff0535fe91faa
= <wfcreations@gmail.com>
2015-08-27 18:53:17 -0300

    add jquery mixitup

---
+++ bundles/globalassets/plugins/JqueryMixItUp.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace wfcreations\metronic\bundles\globalassets\plugins;
+
+use yii\web\AssetBundle;
+
+class JqueryMixItUp extends AssetBundle {
+
+    public $sourcePath = '@patrickkunka/mixitup/build';
+    public $js = [
+        'jquery.mixitup.min.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+}
